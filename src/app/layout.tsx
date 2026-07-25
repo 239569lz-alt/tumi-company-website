8e04e99029bc6f8f7862dab7a1a7f6a1e3ca745c
239569lz-alt <239569lz@gmail.com>
2026-07-25 15:23:55 +0800

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   metadataBase: new URL(
@@ -34,7 +35,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh-CN">
-      <body>{children}</body>
+     <body>
+  		{children}
+  		<Analytics />
+	</body>
     </html>
   );
 }
